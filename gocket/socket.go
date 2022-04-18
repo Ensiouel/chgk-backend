ce5dba150217149ad42fcf65e1f830c7246afc51
Ensiouel <Ensiouel@mail.ru>
2022-04-18 12:27:44 +0500

    socket

--- gocket/socket.go
+++ gocket/socket.go
@@ -1,9 +1,55 @@
 package gocket
 
-type socket struct {
+import (
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+)
+
+type Socket struct {
 	IRoom
+	id     uuid.UUID
+	room   *room
+	conn   *websocket.Conn
+	gocket *Gocket
+	events map[string]EmitterFunc
 }
 
-func Socket() *socket {
-	return &socket{}
+func NewSocket(conn *websocket.Conn, gocket *Gocket) *Socket {
+	return &Socket{
+		id:     uuid.New(),
+		conn:   conn,
+		gocket: gocket,
+	}
+}
+
+func (socket *Socket) Emit(event string, data EmitterData) {
+	if f, ok := socket.events[event]; ok {
+		f(data)
+	}
+}
+
+func (socket *Socket) On(event string, f EmitterFunc) {
+	socket.events[event] = f
+}
+
+func (socket *Socket) read() {
+	defer func() {
+		socket.conn.Close()
+		socket.gocket.disconnect(socket)
+	}()
+	for {
+	}
+}
+
+func (socket *Socket) write() {
+	defer func() {
+		socket.conn.Close()
+		socket.gocket.disconnect(socket)
+	}()
+	for {
+	}
+}
+
+func (socket *Socket) Join(name string) {
+	socket.gocket.join(name, socket)
 }
