bb84d1451d69a9c8ebc164f956fe0fa6301cfa4a
Ensiouel <ensiouel@mail.ru>
2022-04-29 14:48:32 +0500

    switch user roles

--- user.go
+++ user.go
@@ -1,39 +1,45 @@
 package main
 
-import "chgk/gocket"
+import (
+	"chgk/gocket"
+	"time"
+)
 
 type UserRole int
 
 const (
 	UserRoleMaster = UserRole(iota)
-	UserRoleSpectator
 	UserRoleCaptain
 	UserRoleСonnoisseur
+	UserRoleSpectator
 )
 
 func NewUser(id, name string, role UserRole, socket *gocket.Socket, online bool) *User {
 	return &User{
-		ID:     id,
-		Name:   name,
-		Role:   role,
-		Socket: socket,
-		Online: online,
+		ID:             id,
+		Name:           name,
+		Role:           role,
+		Socket:         socket,
+		Online:         online,
+		ConnectionTime: time.Now().Unix(),
 	}
 }
 
 type User struct {
-	ID     string         `json:"id"`
-	Name   string         `json:"name"`
-	Role   UserRole       `json:"role"`
-	Socket *gocket.Socket `json:"-"`
-	Online bool           `json:"online"`
+	ID             string         `json:"id"`
+	Name           string         `json:"name"`
+	Role           UserRole       `json:"role"`
+	Socket         *gocket.Socket `json:"-"`
+	Online         bool           `json:"online"`
+	ConnectionTime int64          `json:"connection_time"`
 }
 
 func (u *User) State() *gocket.EmitterData {
 	return &gocket.EmitterData{
-		"id":     u.ID,
-		"name":   u.Name,
-		"role":   u.Role,
-		"online": u.Online,
+		"id":              u.ID,
+		"name":            u.Name,
+		"role":            u.Role,
+		"online":          u.Online,
+		"connection_time": u.ConnectionTime,
 	}
 }
